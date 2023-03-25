fe6fdc46ad82fe58878209bd4bd9c5c207d9fea5
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-03-25 22:55:26 +0900

    Rails7 ８.２.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      reset_session      # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
@@ -14,5 +17,4 @@ class SessionsController < ApplicationController
 
   def destroy
   end
-
 end
