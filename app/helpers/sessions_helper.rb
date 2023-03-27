0bfc64fc08f8d4b6d69963150cccb1ecfceb27a3
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-03-28 00:50:02 +0900

    Rails7 9.3.2'

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,16 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    # 詳しくは https://bit.ly/33UvK0w を参照
+    session[:session_token] = user.session_token
+  end
+
+  # ユーザーを永続セッションに保存する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   # 永続セッションのためにユーザーをデータベースに記憶する
@@ -13,11 +23,11 @@ module SessionsHelper
   end
 
   # 記憶トークンcookieに対応するユーザーを返す
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.encrypted[:user_id])
-      user = User.find_by(id: user_id)
+def current_user
+if (user_id = session[:user_id])
+@current_user ||= User.find_by(id: user_id)	
+elsif (user_id = cookies.encrypted[:user_id])
+user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
