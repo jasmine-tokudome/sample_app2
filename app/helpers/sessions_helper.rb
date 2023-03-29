cbd3691ebc91388da5e49446992b67bf27b67d1f
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-03-30 01:44:04 +0900

    Rails7 10.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -55,4 +55,9 @@ module SessionsHelper
     @current_user = nil   # 安全のため
   end
 
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
